32ee1857d40757f327364c13a0559c0f2abee7c6
PooyaRezaee <pooya.rezaee.official@gmail.com>
2022-12-31 23:43:59 +0330

    Registred Model Article On admin panel

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import News
+from .models import News,Article
 
 @admin.register(News)
 class NewsModelAdmin(admin.ModelAdmin):
     list_display = ('title','created')
+
+@admin.register(Article)
+class ArticleModelAdmin(admin.ModelAdmin):
+    list_display = ('title','author','created')
